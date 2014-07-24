dbf607ad0323bc8a37a3c5dbb331170c2d81a0d7
Admin <Admin@Principal>
2014-07-23 19:40:54 -0500

    4

--- src/java/pe/edu/upeu/application/dao/DocumentoDAO.java
+++ src/java/pe/edu/upeu/application/dao/DocumentoDAO.java
@@ -7,6 +7,7 @@
 package pe.edu.upeu.application.dao;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import pe.edu.upeu.application.dao_imp.InterfaceDocumentoDAO;
@@ -14,6 +15,8 @@ import pe.edu.upeu.application.factory.ConexionBD;
 import pe.edu.upeu.application.factory.FactoryConnectionDB;
 import pe.edu.upeu.application.model.Datos_Hijo_Trabajador;
 import pe.edu.upeu.application.model.Documentos;
+import pe.edu.upeu.application.model.Padre_Madre_Conyugue;
+import pe.edu.upeu.application.model.Trabajador;
 
 /**
  *
@@ -34,7 +37,7 @@ ConexionBD conn;
                 //falta agregar setter y getter
                 list.add(d);
             }
-        } catch (Exception e) {
+        } catch (SQLException e) {
         }finally{
         this.conn.close();
         }
@@ -58,7 +61,7 @@ ConexionBD conn;
                 d.setTi_documento(rs.getString("ti_documento"));
                 list.add(d);
             }
-        } catch (Exception e) {
+        } catch (SQLException e) {
         }finally{
         this.conn.close();
         }
@@ -92,14 +95,83 @@ ConexionBD conn;
                 dht.setNu_doc(rs.getString("nu_doc"));
                 dht.setUs_creacion(rs.getString("us_creacion"));
                 dht.setUs_modif(rs.getString("us_modif"));
+                list.add(dht);
             }
-        } catch (Exception e) {
+        } catch (SQLException e) {
+        }finally{
+        this.conn.close();
         }
         
-        return null;
+        return list;
         
     }
 
-    
-    
+    @Override
+    public List<Padre_Madre_Conyugue> List_Conyugue(String id_trabajador) {
+        this.conn=FactoryConnectionDB.open(FactoryConnectionDB.ORACLE);
+        String sql = "SELECT * FROM rhtd_padre_madre_conyugue where ap_nombres_conyugue is not null and id_trabajador='"+id_trabajador+"";
+        List<Padre_Madre_Conyugue> list = new ArrayList<Padre_Madre_Conyugue>();
+        try {
+            ResultSet rs = this.conn.query(sql);
+            Padre_Madre_Conyugue pmc = new Padre_Madre_Conyugue();
+            while (rs.next()) {                
+                pmc.setAp_nombres_conyugue(rs.getString("Ap_nombres_conyugue"));
+                pmc.setAp_nombres_madre(rs.getString("ap_nombres_madre"));
+                pmc.setAp_nombres_padre(rs.getString("ap_nombres_padre"));
+                pmc.setEs_trabaja_upeu_conyugue(rs.getString("es_trabaja_upeu_conyugue"));
+                pmc.setFe_creacion(rs.getString("fe_creacion"));
+                pmc.setFe_modif(rs.getString("fe_modif"));
+                pmc.setFe_nac_conyugue(rs.getString("fe_nac_conyugue"));
+                pmc.setId_padre_madre_conyugue(rs.getString("id_padre_madre_conyugue"));
+                pmc.setId_trabajador(rs.getString("id_trabajador"));
+                pmc.setIp_usuario(rs.getString("ip_usuario"));
+                pmc.setLi_inscripcion_vig_essalud(rs.getString("li_inscripcion_vig_essalud"));
+                pmc.setNu_doc(rs.getString("nu_doc"));
+                pmc.setTi_doc_id(rs.getString("Ti_doc_id"));
+                pmc.setUs_creacion(rs.getString("us_creacion"));
+                pmc.setUs_modif(rs.getString("us_modif"));
+                list.add(pmc);
+            }
+        } catch (SQLException e) {    
+        } finally {
+        this.conn.close();
+        }
+        
+        return list;
+    }
+
+    @Override
+    public String List_Adventista(String id_trabajador) {
+          this.conn=FactoryConnectionDB.open(FactoryConnectionDB.ORACLE);
+        String sql = "select li_religion from RHTM_TRABAJADOR where li_religion = 1 and id_trabajador='"+id_trabajador+"'";
+        String religion = "";
+        try {
+            ResultSet rs = this.conn.query(sql);          
+            rs.next();
+            religion = rs.getString("li_religion");
+        } catch (SQLException e) {    
+        } finally {
+        this.conn.close();
+        }
+        
+        return religion;
+    }
+
+    @Override
+    public String List_Req_nacionalidad(String id_trabajador) {
+         this.conn=FactoryConnectionDB.open(FactoryConnectionDB.ORACLE);
+        String sql = "select id_nacionalidad from rhtm_trabajador where id_trabajador not in ( select id_trabajador from rhtm_trabajador where id_nacionalidad = 'NAC-0044') and id_trabajador='"+id_trabajador+"'";
+        String nacionalidad = "";
+        try {
+            ResultSet rs = this.conn.query(sql);          
+            rs.next();
+            nacionalidad = rs.getString("id_nacionalidad");
+        } catch (SQLException e) {    
+        } finally {
+        this.conn.close();
+        }
+        
+        return nacionalidad;
+    }
+     
 }
--- src/java/pe/edu/upeu/application/dao_imp/InterfaceDocumentoDAO.java
+++ src/java/pe/edu/upeu/application/dao_imp/InterfaceDocumentoDAO.java
@@ -9,6 +9,8 @@ package pe.edu.upeu.application.dao_imp;
 import java.util.List;
 import pe.edu.upeu.application.model.Datos_Hijo_Trabajador;
 import pe.edu.upeu.application.model.Documentos;
+import pe.edu.upeu.application.model.Padre_Madre_Conyugue;
+import pe.edu.upeu.application.model.Trabajador;
 
 /**
  *
@@ -19,4 +21,7 @@ public interface InterfaceDocumentoDAO {
     public List<Documentos> List_Documentos();
     //public List<Documentos> List_Doc_Req_Pla(String id_dgp,String id_trabajador);
     public List<Datos_Hijo_Trabajador> List_Hijos(String id_trabajador);
+    public List<Padre_Madre_Conyugue> List_Conyugue(String id_Trabajador);
+    public String List_Adventista (String id_trabajador);
+    public String List_Req_nacionalidad(String id_trabajador);
 }
