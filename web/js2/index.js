494d773306dfd8d9d752d97c81f570ffe8b3cfbb
Docente <Docente@FIA-SISTEMAS01.lim.upeu.edu.pe>
2014-08-04 16:06:49 -0500

    inicializacion en git

--- web/js2/index.js
+++ web/js2/index.js
@@ -1,29 +1,21 @@
 $(document).ready(function(){
     $("#formulario").submit(function(){
-    $("#mensaje").removeClass().addClass('myinfo').text('Validating Your Login ').fadeIn(1000);
-    this.timer = setTimeout(function () {
+    $("#mensaje").removeClass().addClass('myinfo').text('Validando Datos!! ').fadeIn(1000);
+    this.timer = setTimeout(function() {
     $.ajax({
             type: 'POST',
             url: 'valida',
             data: $("#formulario").serialize(),
     success: function(msg){
-    if(msg != 'ERROR') // Message Sent, check and redirect
-    {                // and direct to the success page
-        $("#mensaje").html('Login Verified, Logging in.....').addClass('myinfo').fadeTo(900,1,
-            function()
-            {
-            //redirect to secure page
-            document.location='menu';
-            });
-    }else{
-        $("#mensaje").fadeTo(200,0.1,function() //start fading the messagebox
-        {//add message and change the class of the box and start fading
-        $(this).html('Disculpe. USUARIO Y CLAVE INCORRECTO').removeClass().addClass('myerror').fadeTo(900,1);
-    });
- 
-    }
-    }
- 
+        if(msg != 'ERROR') 
+        {   $("#mensaje").html('Usuario Verificando...!').addClass('myinfo').fadeTo(500,1,
+                function()
+                {      document.location='menu';});
+        }else{
+            $("#mensaje").fadeTo(200,0.1,function()
+            {$(this).html('Disculpe. USUARIO Y CLAVE INCORRECTO').removeClass().addClass('myerror').fadeTo(300,1);});
+        }
+    } 
     });
     }, 200);
     return false;
