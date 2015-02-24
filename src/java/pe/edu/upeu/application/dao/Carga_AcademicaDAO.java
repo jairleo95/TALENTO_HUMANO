337ea9dd455ae5572df013aa8debdf95706c92d5
ALFA 3 <ALFA 3@ALFA3.lim.upeu.edu.pe>
2015-02-24 09:55:50 +0100

    gg

--- src/java/pe/edu/upeu/application/dao/Carga_AcademicaDAO.java
+++ src/java/pe/edu/upeu/application/dao/Carga_AcademicaDAO.java
@@ -5,6 +5,7 @@
  */
 package pe.edu.upeu.application.dao;
 
+import java.sql.CallableStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import pe.edu.upeu.application.dao_imp.InterfaceCarga_AcademicaDAO;
@@ -35,17 +36,63 @@ public class Carga_AcademicaDAO implements InterfaceCarga_AcademicaDAO {
         } catch (SQLException e) {
             throw new RuntimeException(e.getMessage());
         } catch (Exception e) {
-            throw new RuntimeException("ERROR: "+e.getMessage());
+            throw new RuntimeException("ERROR: " + e.getMessage());
         } finally {
             try {
                 this.conn.close();
             } catch (Exception e) {
-                 throw new RuntimeException(e.getMessage());
-                
+                throw new RuntimeException(e.getMessage());
+
             }
         }
         return idtr;
 
+    }
+
+    @Override
+    public void INSERT_CARGA_ACADEMICA(String ID_CARGA_ACADEMICA, String ES_CARGA_ACADEMICA, String CAMPUS, String ES_TIPO_DOC, String NU_DOC, String AP_PATERNO, String AP_MATERNO, String NO_TRABAJADOR, String NO_FACULTAD, String NO_EAP, String DE_CARGA, String NO_CURSO, String NU_GRUPO, String DE_HORARIO, int CA_HLAB, String DE_CONDICION, String DE_TIPO_CURSO, int CA_TIPO_HORA_PAGO, String FE_DESDE, String FE_HASTA, String ES_PROCESADO, String US_CREACION, String FE_CREACION, String US_MODIF, String FE_MODIF, String IP_USUARIO, String NO_USUARIO) {
+        try {
+            this.conn = FactoryConnectionDB.open(FactoryConnectionDB.ORACLE);
+            CallableStatement cst = this.conn.conex.prepareCall("{CALL RHSP_INSERT_RHTM_CARGA_ACADEMICA( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ? )} ");
+            cst.setString(1, null);
+            cst.setString(2, ES_CARGA_ACADEMICA);
+            cst.setString(3, CAMPUS);
+            cst.setString(4, ES_TIPO_DOC);
+            cst.setString(5, NU_DOC);
+            cst.setString(6, AP_PATERNO);
+            cst.setString(7, AP_MATERNO);
+            cst.setString(8, NO_TRABAJADOR);
+            cst.setString(9, NO_FACULTAD);
+            cst.setString(10, NO_EAP);
+            cst.setString(11, DE_CARGA);
+            cst.setString(12, NO_CURSO);
+            cst.setString(13, NU_GRUPO);
+            cst.setString(14, DE_HORARIO);
+            cst.setInt(15, CA_HLAB);
+            cst.setString(16, DE_CONDICION);
+            cst.setString(17, DE_TIPO_CURSO);
+            cst.setInt(18, CA_TIPO_HORA_PAGO);
+            cst.setString(19, FE_DESDE);
+            cst.setString(20, FE_HASTA);
+            cst.setString(21, ES_PROCESADO);
+            cst.setString(22, US_CREACION);
+            cst.setString(23, FE_CREACION);
+            cst.setString(24, US_MODIF);
+            cst.setString(25, FE_MODIF);
+            cst.setString(26, IP_USUARIO);
+            cst.setString(27, NO_USUARIO);
+            cst.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException(e.getMessage());
+        } catch (Exception e) {
+            throw new RuntimeException("ERROR :" + e.getMessage());
+        } finally {
+            try {
+                this.conn.close();
+            } catch (Exception e) {
+                throw new RuntimeException(e.getMessage());
+            }
+        }
     }
 
 }
--- web/index_gg.jsp
+++ web/index_gg.jsp
@@ -1,18 +1,10 @@
 
-<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-         pageEncoding="ISO-8859-1"
-         import="java.util.*"
-         import="org.apache.commons.fileupload.*"
-         import="org.apache.commons.fileupload.servlet.*"
-         import="org.apache.commons.fileupload.disk.*"
-         import="java.io.*"%>
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
         <title>Subidor Devtroce</title>
     </head>
     <body>
-        <%= pageContext.getAttribute("ListaridTrabajador") %>
+        <%= "0010000001000000000000000000000000000000000000000000000000000100000010000000000000000000000000000000000000000000".length() %>
     </body>
 </html>
