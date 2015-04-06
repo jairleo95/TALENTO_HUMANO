540734a1a4d7be77585ca8be29e8356b2be73d3d
Jose <Jose@ALFA2.lim.upeu.edu.pe>
2015-04-06 11:38:28 -0500

    Merge origin/master

--- src/java/pe/edu/upeu/application/dao_imp/InterfaceDetalle_Centro_Costo.java
+++ src/java/pe/edu/upeu/application/dao_imp/InterfaceDetalle_Centro_Costo.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package pe.edu.upeu.application.dao_imp;
 
 /**
@@ -11,6 +10,9 @@ package pe.edu.upeu.application.dao_imp;
  * @author JAIR
  */
 public interface InterfaceDetalle_Centro_Costo {
-    
-     public void INSERT_DETALLE_CENTRO_COSTO(String ID_DETALLE_CENTRO_COSTO, String ID_CENTRO_COSTO, String ID_DGP, Double CA_PORCENTAJE, String IP_USUARIO, String US_CREACION, String FE_CREACION, String US_MODIF, String FE_MODIF,String ID_CONTRATO, String ES_DETALLE_CC);
+
+    public void INSERT_DETALLE_CENTRO_COSTO(String ID_DETALLE_CENTRO_COSTO, String ID_CENTRO_COSTO, String ID_DGP, Double CA_PORCENTAJE, String IP_USUARIO, String US_CREACION, String FE_CREACION, String US_MODIF, String FE_MODIF, String ID_CONTRATO, String ES_DETALLE_CC);
+
+    public void Modificar_Centro_Costo_porc(String id_dcc, Double porc, String Id_user);
+
 }
