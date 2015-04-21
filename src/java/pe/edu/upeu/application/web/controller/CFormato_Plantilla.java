ad28a699ec9bf1f0c8594bd6dc0b465cc35bbf19
Jose <Jose@ALFA2.lim.upeu.edu.pe>
2015-04-21 10:36:21 -0500

    Merge origin/master
    Conflicts:
    	src/java/pe/edu/upeu/application/web/controller/CContrato.java

--- src/java/pe/edu/upeu/application/web/controller/CFormato_Plantilla.java
+++ src/java/pe/edu/upeu/application/web/controller/CFormato_Plantilla.java
@@ -75,11 +75,9 @@ public class CFormato_Plantilla extends HttpServlet {
                     String texto_html = request.getParameter("valor");
                     String id = request.getParameter("id");
                     String ubicacion = "";
-                    /* if (System.getProperty("sun.desktop").trim().equals("windows")) {
-                     ubicacion = direccion_raiz + "\\Vista\\Contrato\\Formato_Plantilla\\Formato\\";
-                     } else {*/
+
                     ubicacion = "/var/lib/tomcat7/webapps/TALENTO_HUMANO/Vista/Contrato/Formato_Plantilla/Formato/";
-               // }
+
                     //ubicacion = getServletContext().getRealPath(".").substring(0, getServletContext().getRealPath(".").length() - 11)+"web\\Vista\\Contrato\\Formato_Plantilla\\Formato";
                     File archivo = new File(ubicacion + id);
                     if (archivo.exists()) {
