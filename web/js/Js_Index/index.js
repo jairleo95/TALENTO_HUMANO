f3c6743236cb3d1708530facd8eafd130739d146
Jairleo95 <Jairleo95@Jair>
2015-07-16 08:20:26 -0500

    a

--- web/js/Js_Index/index.js
+++ web/js/Js_Index/index.js
@@ -1,23 +1,32 @@
-$(document).ready(function(){
-    $("#login-form").submit(function(){
-    $("#mensaje").removeClass().addClass('myinfo').text('Validando Datos... ').fadeIn(50);
-    this.timer = setTimeout(function() {
-    $.ajax({
-            type: 'POST',
-            url: 'valida',
-            data: $("#login-form").serialize(),
-    success: function(msg){
-        if(msg != 'ERROR') 
-        {   $("#mensaje").html('Usuario Verificado!').addClass('myinfo').fadeTo(50,1,
-                function()
-                {      document.location='menu';});
-        }else{
-            $("#mensaje").fadeTo(20,0.1,function()
-            {$(this).html('Disculpe. USUARIO Y CLAVE INCORRECTO').removeClass().addClass('myerror').fadeTo(50,1);});
-            }
-        } 
+$(document).ready(function() {
+    $("#login-form").submit(function() {
+        $("#mensaje").removeClass().addClass('myinfo').text('Validando Datos... ').fadeIn(50);
+        this.timer = setTimeout(function() {
+            $.ajax({
+                type: 'POST',
+                url: 'valida',
+                data: $("#login-form").serialize(),
+                success: function(msg) {
+                    if (msg != 'ERROR') {
+                        $("#mensaje").html('Usuario Verificado!').addClass('myinfo').fadeTo(50, 1,
+                                function() {
+                                    document.location = 'menu';
+                                });
+                    } else {
+                        $("#mensaje").fadeTo(20, 0.1, function()
+                        {
+                            $(this).html('Disculpe. USUARIO Y CLAVE INCORRECTO').removeClass().addClass('myerror').fadeTo(50, 1);
+                        });
+                    }
+                }
+            }).error(function(event, jqXHR, ajaxSettings, thrownError) {
+          //alert(ajaxSettings);
+           $("#mensaje").fadeTo(20, 0.1, function()
+                        {
+                            $(this).html(ajaxSettings).removeClass().addClass('myerror').fadeTo(50, 1);
+                        });
+            });
+        }, 200);
+        return false;
     });
-    }, 200);
-    return false;
-    }); 
-}); 
+});
