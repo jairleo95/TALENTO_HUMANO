421a2b56d1ca5a99e4300ae22a6a5f4277c2f0d8
ALPHA <ALPHA@UPEU.lim.upeu.edu.pe>
2016-10-12 17:48:14 -0500

    Jair Commited

---
+++ src/java/pe/edu/upeu/application/factory/TestWS.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pe.edu.upeu.application.factory;
+
+/**
+ *
+ * @author ALPHA
+ */
+public class TestWS {
+    
+      public static void main(String[] args) throws Exception {
+       WSClienteAcademico.start_ws_academico("2015-1");
+
+    }  
+    
+}
