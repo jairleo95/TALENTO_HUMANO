691d1c605daefcefe5f03816467d000b8c1310b9
ALPHA <ALPHA@UPEU.lim.upeu.edu.pe>
2016-11-08 10:52:23 -0500

    modificacion carga academica

--- src/java/pe/edu/upeu/application/factory/FactoryConnectionDB.java
+++ src/java/pe/edu/upeu/application/factory/FactoryConnectionDB.java
@@ -26,14 +26,14 @@ public class FactoryConnectionDB {
 
     public static String[] configORACLE = {"procesosrh", "rrhh", "192.168.21.12", "1521", "xe"};
 //
-    public static final String url = "/opt/tomcat8/webapps/filesSystemTalentoHumano/";
-    //  public static final String url = "C:\\filesSystemTalentoHumano\\";
+    public static final String url = "/opt/tomcat8/webapps/files/";
+    //  public static final String url = "C:\\fileSystemServer\\web\\filesSystemTalentoHumano\\";
 //public static final String url = "E:\\Projects\\TH\\th 1\\TALENTO_HUMANO\\web\\Archivo";  ESTE URL CAMBIA DE ACUERDO A LA MAQUINA
-    // public static final String url_archivos = "C:\\filesSystemTalentoHumano\\";
+    //public static final String url_archivos = "http://localhost:8082/fileSystemServer/filesSystemTalentoHumano/";
 
     //  public static String[] configORACLE = {"procesosrh", "rrhh", "procesrh-db.upeu", "1521", "upeu"};
 //    public static final String url = "/usr/share/tomcat7/webapps/TALENTO_HUMANO/Archivo/";
-    public static final String url_archivos = "http://procesosrh-gth.upeu/filesSystemTalentoHumano/";
+    public static final String url_archivos = "http://procesosrh-gth.upeu/fileSystemServer/filesSystemTalentoHumano/";
 
     /* WebService Carga Academica*/
     public static final String keyApp = "4a7c88ee0791cad24a15d43a525982f8";
